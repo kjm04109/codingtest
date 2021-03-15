05e704f26248fa7315c1a2f791ecd9089f591a0a
kjm04109 <kjm04109@naver.com>
2021-03-15 18:58:23 +0900

    for+if

--- 210315_for문/210315_10871_X보다_작은_수.py
+++ 210315_for문/210315_10871_X보다_작은_수.py
@@ -4,7 +4,7 @@ n, x =map(int,input().split())
 data= list(map(int,sys.stdin.readline().split()))
 
 
-for i in data:
+for i in data:  #리스트 요소 i로 받아올 때는 그냥 data 
     if i < x:
         print(i,end = " ")  #자동 줄바꿈 안되도록 end 지정!!
     
