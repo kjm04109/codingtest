7f189a1a7627b6b6b5a82a63170081fe1730518e
kjm04109 <kjm04109@naver.com>
2021-03-28 17:06:27 +0900

    while

---
+++ 210328_while문/210328_10952_A+B-5.py
@@ -0,0 +1,10 @@
+import sys
+number= []
+while (True):
+    num = list(map(int,sys.stdin.readline().split()))
+    if num[0]==0 & num[1]==0:
+        break
+    number.append(num)
+
+for i in number:
+    print(i[0]+i[1])
